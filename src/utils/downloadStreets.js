0ecdd16844e37a7564a75bb07823f0c75c2c9833
ptakkk <therealbercik@gmail.com>
2026-03-14 20:33:22 +0100

    added exceptions to street objects

--- src/utils/downloadStreets.js
+++ src/utils/downloadStreets.js
@@ -26,9 +26,23 @@ out geom;
 
   const geo = osmtogeojson(data);
 
+  const excludedHighways = [
+		"platform",
+		"service",
+		"footway",
+		"parking",
+		"path",
+		"cycleway",
+		"pedestrian"
+	];
+
+	geo.features = geo.features.filter(f =>
+		f.properties.name && !excludedHighways.includes(f.properties.highway)
+	);
+
   fs.writeFileSync(
     "./data/streets_poznan.geojson",
-    JSON.stringify(geo)
+    JSON.stringify(geo, null, 2)
   );
 
   console.log("GeoJSON zapisany");
